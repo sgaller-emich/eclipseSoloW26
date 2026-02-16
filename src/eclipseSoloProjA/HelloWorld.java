1b0444e974298b69c50b9abf202c52f58d3d7a12
sgaller-emich <sgaller@emich.edu>
2026-02-16 13:29:48 -0500

    Programmer 1 did this

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github");
 		System.out.println("We're adding another line!");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this, Programmer 1 did this");
 	}
 }
